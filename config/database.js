ccaae2bad7cb30294d7f35e0908f3bc438e938e4
Dennis Gabeler <dc.gabeler@gmail.com>
2017-05-29 02:54:11 +0200

    url update

--- config/database.js
+++ config/database.js
@@ -1,4 +1,9 @@
+var url = 'mongodb://localhost/csgostorage';
+
+if(process.env.MONGO_URL) {
+    url = 'mongodb://magicboy159:Pluisje20088!@ds151661.mlab.com:51661/csgostorage';
+}
+
 module.exports = {
-    //url: 'mongodb://localhost/csgostorage'
-    url: 'mongodb://magicboy159:Pluisje20088!@ds151661.mlab.com:51661/csgostorage'
+    url: url
 }
