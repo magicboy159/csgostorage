06c43092e27f4b48a8fd624e6a954a415ea25dc3
Dennis Gabeler <dc.gabeler@gmail.com>
2017-05-29 02:35:20 +0200

    Changed db url

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost/csgostorage'
+    //url: 'mongodb://localhost/csgostorage'
+    url: 'mongodb://magicboy159:Pluisje20088@ds151661.mlab.com:51661/csgostorage';
 }
